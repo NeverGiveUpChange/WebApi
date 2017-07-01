8bee2b1833c9eedb3c671ccd50906944f2a07366
陈龙 <13220105380@163.com>
2017-07-01 17:21:44 +0800

    增加CacheManager缓存框架，实现多级缓存

---
+++ HangFire_CacheManager/CacheManagerClient.cs
@@ -0,0 +1,66 @@
+﻿using CacheManager.Core;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangFire_CacheManager
+{
+    public class CacheManagerClient
+    {
+        private static readonly ConcurrentDictionary<string, ICacheManager<object>> _connectionCache = new ConcurrentDictionary<string, ICacheManager<object>>();
+        private static readonly object Locker = new object();
+        private static string connectionCacheKey = string.Empty;
+        private CacheManagerClient()
+        { }
+        public static ICacheManager<object> Instance
+        {
+            get
+            {
+                connectionCacheKey = GetConnectionCacheKey();
+                if (_connectionCache[connectionCacheKey] == null)
+                {
+                    lock (Locker)
+                    {
+                        if (_connectionCache[connectionCacheKey] == null)
+                        {
+                            _connectionCache[connectionCacheKey] =
+                            GetCacheManagerClient();
+                        }
+                    }
+                }
+                return _connectionCache[connectionCacheKey];
+
+            }
+        }
+        private static ICacheManager<object> GetCacheManagerClient()
+        {
+                _connectionCache[connectionCacheKey] = CacheFactory.Build(CacheManagerInitParameter.InstanceName, settings =>
+                {
+                    settings.WithUpdateMode(CacheUpdateMode.Up)
+               .WithSystemRuntimeCacheHandle(CacheManagerInitParameter.SystemCacheHandleName)//内存缓存Handle
+               .WithExpiration(ExpirationMode.Sliding, TimeSpan.FromSeconds(60))//默认内存缓存时间
+               .And
+               .WithRedisConfiguration(CacheManagerInitParameter.CacheName, config =>//Redis缓存配置
+           {
+               config.WithAllowAdmin()
+                          .WithDatabase(CacheManagerInitParameter.DbNum)
+                          .WithEndpoint(CacheManagerInitParameter.RedisConnectionString, CacheManagerInitParameter.RedisEndPoint);
+           })
+               .WithMaxRetries(1000)//尝试次数
+               .WithRetryTimeout(100)//尝试超时时间
+               .WithRedisBackplane(CacheManagerInitParameter.CacheName)//redis使用Back Plate
+               .WithRedisCacheHandle(CacheManagerInitParameter.CacheName, true)    //redis缓存handle
+               .WithExpiration(ExpirationMode.Sliding, TimeSpan.FromHours(24));//默认redis缓存时间
+                });
+            return _connectionCache[connectionCacheKey];
+        }
+        private static string GetConnectionCacheKey()
+        {
+            return CacheManagerInitParameter.RedisConnectionString + "_" + CacheManagerInitParameter.RedisEndPoint;
+        }
+
+    }
+}
---
+++ HangFire_CacheManager/CacheManagerInitParameter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangFire_CacheManager
+{
+   public class CacheManagerInitParameter
+    {
+        private static string _cacheName= "redis";
+        private static string _instanceName = "myCache";
+
+        private static string _systemCacheHandleName = "inProcessCache";
+        private static string _redisConnectionString = "127.0.0.1";
+        private static int _redisEndPoint = 6379;
+        private static int _dbNum = 0;
+        /// <summary>
+        /// 缓存管理实例名称
+        /// </summary>
+        public static string InstanceName { get => _instanceName; set => _instanceName = value; }
+        /// <summary>
+        /// 系统内存缓存句柄名称
+        /// </summary>
+        public static string SystemCacheHandleName { get => _systemCacheHandleName; set => _systemCacheHandleName = value; }
+        /// <summary>
+        /// redis连接字符串
+        /// </summary>
+        public static string RedisConnectionString { get => _redisConnectionString; set => _redisConnectionString = value; }
+        /// <summary>
+        /// redis端口
+        /// </summary>
+        public static int RedisEndPoint { get => _redisEndPoint; set => _redisEndPoint = value; }
+        /// <summary>
+        /// redis存储Db号
+        /// </summary>
+        public static int DbNum { get => _dbNum; set => _dbNum = value; }
+        public static string CacheName { get => _cacheName; private set => _cacheName = value; }
+    }
+}
